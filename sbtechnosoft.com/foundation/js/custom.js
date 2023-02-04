1530652465dcf720e17f2772eaa9f4af48faed9a
Bethan Helen <bethanhelen8@gmail.com>
2023-02-05 00:51:46 +0100

    first commit

---
+++ sbtechnosoft.com/foundation/js/custom.js
@@ -0,0 +1,181 @@
+(function($) {
+    'use strict'; // Start of use strict
+
+    /*------------------------------------------------------------------
+     	Scrool Top
+     ------------------------------------------------------------------*/
+    $.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    });
+
+    /*------------------------------------------------------------------
+        Header Navigation
+    ------------------------------------------------------------------*/
+    var windowSize = $(window).width();
+
+    if (windowSize >= 767) {
+        $('ul.nav li.dropdown').hover(function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+        }, function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+        });
+    }
+    /*------------------------------------------------------------------
+        Loader
+    ------------------------------------------------------------------*/
+
+    $("#dvLoading").fadeOut("fast");
+
+    // map zooming 	
+    $('.google-map').on('click', function() {
+        $('.google-map').find('iframe').css("pointer-events", "auto");
+    });
+
+    /*------------------------------------------------------------------
+        Gallery
+    ------------------------------------------------------------------*/
+    $("div[id^='myModal']").each(function() {
+
+        var currentModal = $(this);
+
+        //click next
+        currentModal.find('.btn-next').click(function() {
+            currentModal.modal('hide');
+            var modalid = $(this).attr('data-modal')
+            $("div[data-modal='" + modalid + "']").first().modal('show');
+        });
+
+        //click prev
+        currentModal.find('.btn-prev').click(function() {
+            currentModal.modal('hide');
+            var modalid = $(this).attr('data-modal')
+            $("div[data-modal='" + modalid + "']").first().modal('show');
+        });
+
+    });
+
+    /*------------------------------------------------------------------
+    Owl Carousel for screenshots
+	------------------------------------------------------------------*/
+    var owl = $("#testimonials");
+    owl.owlCarousel({
+        nav: true,
+        margin: 10,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            450: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            700: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            },
+            1400: {
+                items: 1
+            },
+            1600: {
+                items: 1
+            }
+        }
+    });
+    $(".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+    $(".owl-next").html('<i class="fa fa-chevron-right"></i>');
+
+    /*------------------------------------------------------------------
+    FAQ
+    ------------------------------------------------------------------*/
+    $('.panel-heading a').on('click', function() {
+        $('.panel-heading').removeClass('active');
+        $(this).parents('.panel-heading').addClass('active');
+    });
+    /*------------------------------------------------------------------
+     Accordion Box
+     ------------------------------------------------------------------*/
+    if ($('.accordion-box').length) {
+        $(".accordion-box").on('click', '.acc-btn', function() {
+
+            var outerBox = $(this).parents('.accordion-box');
+            var target = $(this).parents('.accordion');
+
+            if ($(this).hasClass('active') !== true) {
+                $('.accordion .acc-btn').removeClass('active');
+            }
+
+            if ($(this).next('.acc-content').is(':visible')) {
+                return false;
+            } else {
+                $(this).addClass('active');
+                $(outerBox).children('.accordion').removeClass('active-block');
+                $(outerBox).find('.accordion').children('.acc-content').slideUp(300);
+                target.addClass('active-block');
+                $(this).next('.acc-content').slideDown(300);
+            }
+        });
+    }
+    /*------------------------------------------------------------------
+        Animation Numbers
+    ------------------------------------------------------------------*/
+    jQuery('.animateNumber').each(function() {
+        var num = jQuery(this).attr('data-num');
+
+        var top = jQuery(document).scrollTop() + (jQuery(window).height());
+        var pos_top = jQuery(this).offset().top;
+        if (top > pos_top && !jQuery(this).hasClass('active')) {
+            jQuery(this).addClass('active').animateNumber({
+                number: num
+            }, 2000);
+        }
+    });
+
+    /*------------------------------------------------------------------
+     Count Down
+    ------------------------------------------------------------------*/
+    if ($(".count-down").length) {
+        var year = parseInt($(".count-down").attr("data-countdown-year"), 10);
+        var month = parseInt($(".count-down").attr("data-countdown-month"), 10) - 1;
+        var day = parseInt($(".count-down").attr("data-countdown-day"), 10);
+        $(".count-down").countdown({
+            until: new Date(year, month, day),
+            padZeroes: true
+        });
+    }
+
+    /*------------------------------------------------------------------
+    Video Popup
+    ------------------------------------------------------------------*/
+    var $popupVideo = $('[data-popup="video"]');
+
+    if ($popupVideo.length) {
+        $popupVideo.magnificPopup({
+            type: 'iframe'
+        });
+    }
+
+})(jQuery);
+
+/*------------------------------------------------------------------
+Count Down
+------------------------------------------------------------------*/
+if ($(".count-down").length) {
+    var year = parseInt($(".count-down").attr("data-countdown-year"), 10);
+    var month = parseInt($(".count-down").attr("data-countdown-month"), 10) - 1;
+    var day = parseInt($(".count-down").attr("data-countdown-day"), 10);
+    $(".count-down").countdown({
+        until: new Date(year, month, day),
+        padZeroes: true
+    });
+}
